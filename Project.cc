817cd5625c3ef3462ee2725b949b9a419781f4ca
Brandon Long <blong032593@gmail.com>
2016-11-08 00:26:25 -0500

    Began implementing twist

--- Project.cc
+++ Project.cc
@@ -6,13 +6,19 @@ void int main (){
 	//edge orientation is either flipped [1] or not flipped [0]
 	char edgesOrientation[] = [0,0,0,0,0,0,0,0,0,0,0,0];
 	char edgePosition[] = [0,0,0,0,0,0,0,0,0,0,0,0];
+
+	char edge[12][2];
 	//corner orientation goes 0,1,2
 	char cornerOrientation[] = [0,0,0,0,0,0,0,0];
 	char cornerPosition[] = [0,0,0,0,0,0,0,0];
 
+	char corner[8][2];
 	//using multidimensional array 6 columns(faces) 8 rows(corners/edges)?
-	char mArray[6][8];
+	//char mArray[6][8];
 	
+	//corner[0][0] is the position of first corner
+	//corner[0][1] is the orientation of first corner
+
 }
 /*
 indexes start at top back left and go clockwise
@@ -20,7 +26,7 @@ face 0 would be corner index 2,3,6,7
 	edge index 2,6,7,10
 	it would look like the following:
 		[2][2][3]
-		[6][ ][7]
+		[6][0][7]
 		[6][10][7]
 
 
@@ -35,24 +41,41 @@ face 0 would be corner index 2,3,6,7
 
 
 
-void twistClockwise(char* array[],char face) {
+void twistClockwise(char* corners, char* edges,int face) {
+	if(face == 0){
+		char temp = corners[1][0];
+		corner[1][0] = corner[5][0];
+		corner[5][0] = corner[6][0];
+		corner[6][0] = corner[2][0];
+		corner[2][0] = temp;
+		temp = edge[1][0];
+		edge[1][0] = edge[5][0];
+		edge[5][0] = edge[9][0];
+		edge[9][0] = edge[6][0];
+		edge[6][0] = temp;
+	}
+
+
+
+/*
+	-----pseudo stuff-----
 	if face = 0 //top face
 	temp = corner 2
-	corner 2 = corner6
+	corner 2 = corner 6
 	corner 6 = corner 7
 	corner 7 = corner 3
 	corner 3 = temp
-	temp = edge 2
-	edge 2 = edge 6
-	edge 6 = edge 10
-	edge 10 = edge 7
-	edge 7 = temp
+	temp     = edge 2
+	edge 2   = edge 6
+	edge 6   = edge 10
+	edge 10  = edge 7
+	edge 7   = temp
 
 	edge/corner orientation unaffected
 	edges move
 
 	if face = 2 or 4
 	edges flip orientation
-	
+*/
 	
 }
