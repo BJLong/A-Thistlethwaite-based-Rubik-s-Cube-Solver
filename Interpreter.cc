b82b7c0198f94d216a98a7c6062c9dc066d64be1
Brandon Long <blong032593@gmail.com>
2016-12-11 18:58:09 -0500

    a

--- Interpreter.cc
+++ Interpreter.cc
@@ -140,7 +140,14 @@ void makeCube(cube c){
 		for (int j = 0; j < 12; j++){
 			if( code == edgeMap[j]){
 				edges[0][i] = j;
+
 			}
+			if (facelet[edgeFacelets[i][0]] == edgeBadColors[i][0] || 
+			facelet[edgeFacelets[i][0]] == edgeBadColors[i][1] || 
+			facelet[edgeFacelets[i][1]] == edgeBadColors[i][2] || 
+			facelet[edgeFacelets[i][1]] == edgeBadColors[i][3]){
+				edges[1][i] = 1;
+			}else{edges[1][i] = 0;}
 		}
 		
 	}
