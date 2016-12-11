fc8451b1a25bca9bf9da4f09c2137fe6bef87c2a
Brandon Long <blong032593@gmail.com>
2016-12-10 23:06:52 -0500

    rotateleft

--- Interpreter.cc
+++ Interpreter.cc
@@ -10,34 +10,34 @@ char rotateUpArray[54] = {
 	17,16,15,14,13,12,11,10,9,
 	27,28,29,30,31,32,33,34,35,
 };
+
+char rotateLeftArray[54] = {
+	9,10,11,12,13,14,15,16,17,
+	18,19,20,21,22,23,24,25,26,
+	27,28,29,30,31,32,33,34,35,
+	0,1,2,3,4,5,6,7,8,9,
+	42,39,36,43,40,37,44,41,38,
+	47,50,53,46,49,52,45,48,51
+};
+
 char facelet[54];
 
 void rotateUp(){
-	temp = facelet;
+	for(int i = 0; i < 54; i++){
+	temp[i] = facelet[i];
+	}
 	for(int i = 0; i < 54; i++){
 		facelet[i] = temp[rotateUpArray[i]];
 	}
 }
 
-// ignore for now
 void rotateLeft(){
-	char temp[9];
-	for(int i = 0; i < 9; i++){
-		temp[i] = facelet[i];
+	for(int i = 0; i < 54; i++){
+	temp[i] = facelet[i];
 	}
-	for(int i = 0; i < 9; i++){
-		temp[i] = facelet[i+9];
-	}
-	for(int i = 0; i < 9; i++){
-		temp[i+9] = facelet[i+18];
-	}
-	for(int i = 0; i < 9; i++){
-		temp[i+18] = facelet[i+27];
-	}
-	for(int i = 0; i < 9; i++){
-		facelet[i+27] = temp[i];
-	}
-
+	for(int i = 0; i < 54; i++){
+		facelet[i] = temp[rotateLeftArray[i]];
+	}	
 }
 
 int main(){
